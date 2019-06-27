bc82b13aeefa0d13f05d47fcca37be72db6cd375
Nikolay Radonich <nikolayradonich@gmail.com>
2019-06-27 22:31:27 +0300

    made the nav work

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React from "react";
+import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Calendar from "./components/Calendar";
 
-function App() {
-  return (
-    <div className="App">
-      <h1 className="display-4 m-2">Calendar App</h1>
-      <Calendar />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1 className="display-4 m-2">Calendar App</h1>
+        <Calendar />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -2,8 +2,9 @@ import React, { Component } from "react";
 import "./index.css";
 class Calendar extends Component {
   state = {
-    current: new Date()
-    // immutable: new Date()
+    current: new Date(),
+    immutable: new Date(),
+    note: ""
   };
   months = [
     "Jan",
@@ -27,6 +28,21 @@ class Calendar extends Component {
   currentDate = () => this.state.current.getDate();
   firstDay = () =>
     new Date(this.year(), this.state.current.getMonth(), 0).getDay();
+
+  nextMonth = () => {
+    let d = new Date(this.state.current.getTime());
+    let current = new Date(d.setMonth(d.getMonth() + 1));
+    this.setState({ current });
+  };
+  currentMonth = () => {
+    let current = new Date(this.state.immutable.getTime());
+    this.setState({ current });
+  };
+  prevMonth = () => {
+    let d = new Date(this.state.current.getTime());
+    let current = new Date(d.setMonth(d.getMonth() - 1));
+    this.setState({ current });
+  };
   render() {
     let weekdays = this.weekdays.map(day => {
       return (
@@ -36,7 +52,6 @@ class Calendar extends Component {
       );
     });
     let blanks = [];
-    console.log(this.month());
     for (let i = 0; i < this.firstDay(); i++) {
       blanks.push(
         <td className="blank" key={i}>
@@ -48,7 +63,7 @@ class Calendar extends Component {
     for (let i = 1; i <= this.daysInMonth(); i++) {
       let className = i === this.currentDate() ? "day current" : "day";
       daysInMonth.push(
-        <td key={i * 125} className={className}>
+        <td key={i * 125} className={className} onDoubleClick={this.addNote}>
           {i}
         </td>
       );
@@ -77,10 +92,17 @@ class Calendar extends Component {
         <table className="table table-bordered">
           <thead className="thead-dark">
             <tr>
-              <th className="table-nav">prev</th>
               <th colSpan="3">{this.month()}</th>
-              <th colSpan="2">{this.year()}</th>
-              <th className="table-nav">next</th>
+              <th colSpan="1">{this.year()}</th>
+              <th className="table-nav" onClick={this.prevMonth}>
+                <span className="arrows">🢐</span>
+              </th>
+              <th className="table-nav" onClick={this.currentMonth}>
+                today
+              </th>
+              <th className="table-nav" onClick={this.nextMonth}>
+                <span className="arrows">🢒</span>
+              </th>
             </tr>
           </thead>
           <tbody>
