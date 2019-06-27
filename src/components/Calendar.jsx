8d6d9d08fc3939805a9c06f957eb54926f5e9f70
Nikolay Radonich <nikolayradonich@gmail.com>
2019-06-27 13:52:30 +0300

    changed output method

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -4,18 +4,28 @@ moment.updateLocale("en", {
   weekdaysShort: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
 });
 class Calendar extends Component {
-  state = {};
+  state = {
+    dateObj: moment()
+  };
+  firstDay = () => {
+    let date = this.state.dateObj;
+    let firstDay = date.startOf("month").format("d");
+    return firstDay;
+  };
   weekdays = moment.weekdaysShort();
   render() {
+    let weekdays = this.weekdays.map(day => {
+      return (
+        <th key={day} className="week-day">
+          {day}
+        </th>
+      );
+    });
     return (
       <div className="container col-sm-4 mt-3">
-        <table className="table">
+        <table className="table table-bordered">
           <thead className="thead-dark">
-            {this.weekdays.map(day => (
-              <th key={day} className="week-day">
-                {day}
-              </th>
-            ))}
+            <tr>{weekdays}</tr>
           </thead>
         </table>
       </div>
