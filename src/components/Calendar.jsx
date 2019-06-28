6d094ff82a0167dd7d0878075f97abd782a88a5c
Nikolay Radonich <nikolayradonich@gmail.com>
2019-06-28 15:34:01 +0300

    added modal window for editing/creating notes(events)

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,13 +1,29 @@
 import React, { Component } from "react";
 import "./index.css";
 import Cell from "./Cell";
+import ModalWindow from "./ModalWindow";
 class Calendar extends Component {
   state = {
     notes: [],
     current: new Date(),
     immutable: new Date(),
+    modalClass: "modal",
+    currentNoteId: 0,
+    currentNoteText: "",
     note: ""
   };
+  //get data from localStorage
+  componentDidMount() {
+    const notes = JSON.parse(window.localStorage.getItem("notes"));
+    if (notes) this.setState({ notes });
+  }
+  //put data in localStorage
+  componentDidUpdate() {
+    const state = [...this.state.notes];
+    window.localStorage.setItem("notes", JSON.stringify(state));
+  }
+  //arrays of the weekdays and months (could be translated in any languages)
+  weekdays = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
   months = [
     "Jan",
     "Feb",
@@ -22,14 +38,13 @@ class Calendar extends Component {
     "Nov",
     "Dec"
   ];
-  weekdays = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
-  year = () => this.state.current.getFullYear();
-  getMonth = () => this.state.current.getMonth();
-  month = () => this.months[this.getMonth()];
+  year = () => this.state.current.getFullYear(); //get the year [2019]
+  getMonth = () => this.state.current.getMonth(); // get the number of month [5] - June
+  month = () => this.months[this.getMonth()]; // get the name of month - June
   daysInMonth = () =>
-    new Date(this.year(), this.state.current.getMonth() + 1, 0).getDate(); // new Date(year(), this.state.current.getMonth() + 1, 0).getDate()) /////
-  currentDate = () => this.state.current.getDate();
+    new Date(this.year(), this.state.current.getMonth() + 1, 0).getDate(); // get total days in month
   isCurrentDay = day => {
+    //check if the day in loop is current; if true - highlight
     let date = new Date();
     return (
       new Date(this.year(), this.getMonth(), day).getTime() ===
@@ -37,46 +52,69 @@ class Calendar extends Component {
     );
   };
   firstDay = () =>
-    new Date(this.year(), this.state.current.getMonth(), 0).getDay();
+    new Date(this.year(), this.state.current.getMonth(), 0).getDay(); //number of first day in month
 
   nextMonth = () => {
+    //add one month to current
     let d = new Date(this.state.current.getTime());
     let current = new Date(d.setMonth(d.getMonth() + 1));
     this.setState({ current });
   };
   currentMonth = () => {
+    //return to current month
     let current = new Date(this.state.immutable.getTime());
     this.setState({ current });
   };
   prevMonth = () => {
+    //subtract one month from current
     let d = new Date(this.state.current.getTime());
     let current = new Date(d.setMonth(d.getMonth() - 1));
     this.setState({ current });
   };
   getIndex = id => {
+    //find the index of day object by id (searching in the total array of notes)
     return this.state.notes.findIndex(x => x.id === id);
+  };
+  closeModal = () => {
+    const modalClass = "modal";
+    this.setState({ modalClass });
+  };
+  handleSubmit = (e, id) => {
+    e.preventDefault();
+    const notes = [...this.state.notes];
+    const index = this.getIndex(id);
+    if (index !== -1) {
+      if (this.state.note !== "") {
+        notes[index].text = this.state.note;
+      }
+    } else {
+      const newItem = {
+        id,
+        text: this.state.note
+      };
+      notes.push(newItem);
+    }
+    this.setState({ notes, note: "" });
+    this.closeModal();
+  };
+  handleChange = e => {
+    this.setState({ note: e.target.value });
   };
   handleNote = id => {
     const notes = [...this.state.notes];
     const index = this.getIndex(id);
     if (index !== -1) {
-      let text = prompt("What did you planned?", notes[index].text);
-      if (text != null) {
-        notes[index].text = text;
-      }
+      const currentNoteText = notes[index].text;
+      this.setState({ currentNoteText });
     } else {
-      let text = prompt("What did you planned?", "");
-      if (text != null) {
-        const newItem = {
-          id,
-          text
-        };
-        notes.push(newItem);
-      }
+      this.setState({ currentNoteText: "" });
     }
-    this.setState({ notes });
+    const modalClass = "modal visible";
+    this.setState({ currentNoteId: id, modalClass });
   };
   render() {
+    //place the modal window
+    //map the weekdays
     let weekdays = this.weekdays.map(day => {
       return (
         <th key={day} className="week-day">
@@ -86,6 +124,7 @@ class Calendar extends Component {
     });
     let blanks = [];
     for (let i = 0; i < this.firstDay(); i++) {
+      //get an array of days before the first day in month
       blanks.push(
         <td className="blank" key={i}>
           {""}
@@ -94,8 +133,9 @@ class Calendar extends Component {
     }
     let daysInMonth = [];
     for (let i = 1; i <= this.daysInMonth(); i++) {
+      //fill array with all numbers in month
       const id = new Date(this.year(), this.getMonth(), i).getTime();
-      let className = this.isCurrentDay(i) ? "day current" : "day";
+      let className = this.isCurrentDay(i) ? "day current" : "day"; //check if the current day
       const index = this.getIndex(id);
       if (index !== -1 && this.state.notes[index].text !== "") {
         className += " event";
@@ -111,10 +151,11 @@ class Calendar extends Component {
         />
       );
     }
-    let totalDays = [...blanks, ...daysInMonth];
+    let totalDays = [...blanks, ...daysInMonth]; //combine blank and normal days
     let rows = [],
       cells = [];
     totalDays.forEach((row, i) => {
+      // separate all the days into rows and push them to final array
       if (i % 7 !== 0) cells.push(row);
       else {
         let insRow = cells.slice();
@@ -127,11 +168,20 @@ class Calendar extends Component {
         rows.push(insRows);
       }
     });
+    //map the rows (weeks)
     let trElems = rows.map((d, i) => {
       return <tr key={i * 25}>{d}</tr>;
     });
     return (
       <div className="container col-sm-12 col-md-4 mt-3">
+        <ModalWindow
+          modalClass={this.state.modalClass}
+          handleSubmit={this.handleSubmit}
+          handleChange={this.handleChange}
+          noteId={this.state.currentNoteId}
+          closeModal={this.closeModal}
+          noteText={this.state.currentNoteText}
+        />
         <table className="table table-bordered">
           <thead className="thead-dark">
             <tr>
---
+++ src/components/ModalWindow.jsx
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+class ModalWindow extends Component {
+  state = {};
+  componentDidUpdate = () => {
+    console.log("mounted the window");
+  };
+  render() {
+    const { modalClass, noteId, noteText } = this.props;
+    return (
+      <div className={modalClass}>
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title">Your note text</h5>
+              <button
+                type="button"
+                className="close"
+                onClick={this.props.closeModal}
+              >
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div className="modal-body">
+              <p>{noteText}</p>
+              <form onSubmit={e => this.props.handleSubmit(e, noteId)}>
+                <div className="form-group text-left">
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Note text"
+                    onChange={this.props.handleChange}
+                  />
+                </div>
+                <div className="modal-footer">
+                  <button type="submit" className="btn btn-primary">
+                    Save
+                  </button>
+                  <button
+                    type="button"
+                    className="btn btn-secondary"
+                    onClick={this.props.closeModal}
+                  >
+                    Close
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ModalWindow;
